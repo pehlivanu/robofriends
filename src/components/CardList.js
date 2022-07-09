635d4667169758b644222d7daafd0fc8660d3de5
Liviu Ionesi <pehlivanu@gmail.com>
2022-07-09 17:24:25 +0200

    Added error boundry and github pages support

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+  
+  /*
+  if (true){
+    throw new Error('UPS the site is down now');
+  }
+  */
+
   return (
     <div>
       {
--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 // registerServiceWorker();
